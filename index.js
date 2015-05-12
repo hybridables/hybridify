270fc20be137ee41ac92c758b83fc8da0cda95d3
tunnckoCore <mameto_100@mail.bg>
2015-05-12 09:02:47 +0300

    refactor and update

--- index.js
+++ index.js
@@ -1,67 +1,31 @@
-/**
- * hybridify <https://github.com/tunnckoCore/hybridify>
+/*!
+ * hybridify <https://github.com/hybridables/hybridify>
  *
  * Copyright (c) 2015 Charlike Mike Reagent, contributors.
  * Released under the MIT license.
  */
 
-'use strict';
+'use strict'
 
-var handleCallback = require('handle-callback');
-var handleArguments = require('handle-arguments');
-var isEmptyFunction = require('is-empty-function');
-var thenify = require('thenify');
+var Bluebird = require('bluebird')
+var handleArguments = require('handle-arguments')
 
-/**
- * Building hybrid APIs. You can use both callback and promise in same time.
- * Like `asyncFn(name, cb).then().catch()`
- *
- * **Example:**
- *
- * ```js
- * var hybridify = require('hybridify');
- *
- * var hybrid = hybridify(function asyncFn(a, b, c, callback) {
- *   callback(null, a, b, c);
- * });
- *
- * // both in same time!
- * hybrid(1, 2, 3, function(err, res) {
- *   console.log('CALLBACK err:', err);
- *   console.log('CALLBACK res:', res);
- * })
- * .then(function(res) {
- *   console.log('PROMISE res:', res);
- * })
- * .catch(function(err) {
- *   console.log('PROMISE err:', err);
- * })
- * ```
- *
- * @name hybridify
- * @param  {Function} `<asyncFn>` function to hybridify
- * @return {Function} when funcion is called return promise
- * @api public
- */
-var hybridify = module.exports = hybridify;
+module.exports = hybridify
 
-function hybridify(asyncFn) {
-  if (typeof asyncFn !== 'function') {
-    throw new TypeError('hybridify: expect `asyncFn` to be function');
-  }
+function hybridify (asyncFn) {
+  function hybridifyFn () {
+    var argz = handleArguments(arguments)
+    var fn = Bluebird.promisify(asyncFn)
+    var promise = fn.apply(fn, argz.args)
 
-  function hybridifyFn() {
-    var argz = handleArguments(arguments);
-    var cb = argz.callback;
+    promise.hybridify = hybridify
 
-    var promise = thenify(asyncFn).apply(null, argz.args);
-    if (!isEmptyFunction(cb) && cb.name !== 'defaultHandleArgumentsCallback') {
-      promise = handleCallback(promise, cb);
+    if (argz.callback) {
+      return promise.nodeify(argz.callback)
     }
-    promise.hybridify = hybridify;
-    return promise;
+    return promise
   }
 
   hybridifyFn.hybridify = hybridify
-  return hybridifyFn;
+  return hybridifyFn
 }
--- test.js
+++ test.js
@@ -1,147 +1,139 @@
-/**
- * hybridify <https://github.com/tunnckoCore/hybridify>
+/*!
+ * hybridify <https://github.com/hybridables/hybridify>
  *
  * Copyright (c) 2015 Charlike Mike Reagent, contributors.
  * Released under the MIT license.
  */
 
-'use strict';
+'use strict'
 
-var hybridify = require('./index');
+var test = require('assertit')
+var hybridify = require('./index')
 
-var handleCallback = require('./index');
-var assert = require('assert');
-var got = require('got');
+var got = require('got')
+var hybridGot = hybridify(got.get)
 
-var hybridGot = hybridify(got.get);
+test('hybridify:', function () {
+  test('should work', function () {
+    test('with callback api', function (done) {
+      // this.timeout(10000)
 
-// almost the same tests as `handle-callback`,
-// which is expected, because its the core of `hybridify`
-
-describe('hybridify:', function() {
-  describe('should throw error', function() {
-    it('when first argument not a Function', function(done) {
-      function fixture() {
-        return hybridify('expects only function');
-      }
-      assert.throws(fixture, TypeError);
-      done();
-    });
-  });
-
-  describe('should work', function() {
-    it('with callback api', function(done) {
-      this.timeout(10000);
-
-      var hybrid = hybridGot('https://github.com', function(err, res) {
-        var body = res[0];
-        var stream = res[1];
+      hybridGot('https://github.com', function (err, res) {
+        var body = res[0]
+        var stream = res[1]
         // callback api
 
-        assert.strictEqual(err, null);
-        assert(res);
-        assert(stream);
-        assert.strictEqual(body[0], '<'); // it is html
-        done();
+        test.assert.strictEqual(err, null)
+        test.assert.ok(res)
+        test.assert.ok(stream)
+        test.assert.strictEqual(body[0], '<') // it is html
+        done()
       })
-    });
+    })
 
-    it('with promise api', function(done) {
-      this.timeout(10000);
+    test('with promise api', function (done) {
+      // this.timeout(10000)
 
-      var hybrid = hybridGot('https://github.com');
-      hybrid.then(function(res) {
-        var body = res[0];
-        var stream = res[1];
+      hybridGot('https://github.com')
+      .then(function (res) {
+        var body = res[0]
+        var stream = res[1]
         // promise api
 
-        assert(res);
-        assert(stream);
-        assert.strictEqual(body[0], '<'); // it is html
-        done();
-      });
-    });
+        test.assert.ok(res)
+        test.assert.ok(stream)
+        test.assert.strictEqual(body[0], '<') // it is html
+        done()
+      })
+    })
 
-    it('with both callback and promise api', function(done) {
-      this.timeout(10000);
+    test('with both callback and promise api', function (done) {
+      // this.timeout(10000)
+      var i = 0
 
-      var hybrid = hybridGot('https://github.com', function(err, res) {
-        var body = res[0];
-        var stream = res[1];
+      hybridGot('https://github.com', function (err, res) {
+        var body = res[0]
+        var stream = res[1]
         // callback api
-
-        assert.strictEqual(err, null);
-        assert(res);
-        assert(stream);
-        assert.strictEqual(body[0], '<'); // it is html
+        i++
+        test.assert.strictEqual(err, null)
+        test.assert.ok(res)
+        test.assert.ok(stream)
+        test.assert.strictEqual(body[0], '<') // it is html
       })
-      .then(function(res) {
-        var body = res[0];
-        var stream = res[1];
+      .then(function (res) {
+        var body = res[0]
+        var stream = res[1]
         // promise api
+        test.assert.strictEqual(i, 1)
+        test.assert.ok(res)
+        test.assert.ok(stream)
+        test.assert.strictEqual(body[0], '<') // it is html
+        done()
+      })
+    })
+  })
 
-        assert(res);
-        assert(stream);
-        assert.strictEqual(body[0], '<'); // it is html
-        done();
-      });
-    });
-  });
+  test('should be able to create own hybrids', function () {
+    test('every hybrid have `.hybridify` method', function (done) {
+      var hybrid = hybridGot('https://github.com')
 
-  describe('should be able to create own hybrids', function() {
-    it('every hybrid have `.hybridify` method', function(done) {
-      var hybrid = hybridGot('https://github.com');
+      test.assert.strictEqual(typeof hybrid.hybridify, 'function')
+      test.assert.strictEqual(typeof hybridGot.hybridify, 'function')
+      done()
+    })
+    test('every hybrid is promise and have `.then` and `.catch` methods', function (done) {
+      var hybrid = hybridGot('https://github.com')
 
-      assert.strictEqual(typeof hybrid.hybridify, 'function');
-      assert.strictEqual(typeof hybridGot.hybridify, 'function');
-      done();
-    });
-    it('every hybrid is promise and have `.then` and `.catch` methods', function(done) {
-      var hybrid = hybridGot('https://github.com');
+      test.assert.strictEqual(typeof hybrid.then, 'function')
+      test.assert.strictEqual(typeof hybrid.catch, 'function')
+      test.assert.strictEqual(typeof hybrid.hybridify, 'function')
+      test.assert.strictEqual(typeof hybridGot.hybridify, 'function')
+      done()
+    })
+  })
 
-      assert.strictEqual(typeof hybrid.then, 'function');
-      assert.strictEqual(typeof hybrid.catch, 'function');
-      assert.strictEqual(typeof hybrid.hybridify, 'function');
-      assert.strictEqual(typeof hybridGot.hybridify, 'function');
-      done();
-    });
-  });
+  test('should be able to catch error', function () {
+    test('with callback api', function (done) {
+      // this.timeout(10000)
 
-  describe('should be able to catch error', function() {
-    it('with callback api', function(done) {
-      this.timeout(10000);
-
-      var hybrid = hybridGot('https://gitfsdfsdfm', function(err, res) {
+      hybridGot('https://gitfsdfsdfm', function (err, res) {
         // callback api
-        assert.throws(err, Error);
-        assert.strictEqual(res, null || undefined);
-        done();
+        test.assert.ifError(!err)
+        test.assert.ok(err.message)
+        test.assert.strictEqual(res, null || undefined)
+        done()
       })
-    });
+    })
 
-    it('with promise api - with .catch(err)', function(done) {
-      this.timeout(10000);
+    test('with promise api - with .catch(err)', function (done) {
+      // this.timeout(10000)
 
-      var hybrid = hybridGot('https://gitfsdfsdfm')
-      hybrid.catch(function(err) {
+      hybridGot('https://gitfsdfsdfm')
+      .catch(function (err) {
         // promise api
-        assert.throws(err, Error);
-        done();
+        test.assert.ifError(!err)
+        test.assert.ok(err.message)
+        done()
       })
-    });
+    })
 
-    it('with both callback and promise api', function(done) {
-      this.timeout(10000);
-
-      var hybrid = hybridGot('https://gitfsdfsdfm', function(err) {
+    test('with both callback and promise api', function (done) {
+      // this.timeout(10000)
+      var i = 0
+      hybridGot('https://gitfsdfsdfm', function (err) {
         // callback api
-        assert.throws(err, Error);
+        i++
+        test.assert.ifError(!err)
+        test.assert.ok(err.message)
       })
-      .catch(function(err) {
+      .catch(function (err) {
         // promise api
-        assert.throws(err, Error);
-        done();
+        test.assert.strictEqual(i, 1);
+        test.assert.ifError(!err)
+        test.assert.ok(err.message)
+        done()
       })
-    });
-  });
-});
+    })
+  })
+})
