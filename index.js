fffbc974f5984071ca5bd4a4afd5a5ebdf704f41
tunnckoCore <mameto_100@mail.bg>
2016-05-15 16:54:24 +0300

    implement v2

--- index.js
+++ index.js
@@ -7,6 +7,32 @@
 
 'use strict'
 
-module.exports = function hybridify () {
-  // body
+var utils = require('./utils')
+
+var hybridify = module.exports = function hybridify (fn) {
+  utils.letta.Promise = hybridify.Promise
+
+  var names = utils.commonCallbackNames
+  var argz = utils.handle(arguments, [null].concat(names))
+  var promise = utils.letta.apply(this, arguments)
+  var resolved = utils.then(promise).then(argz.callback)
+
+  return utils.normalizePromise(resolved, promise)
+}
+
+hybridify.hybridify = function hybridifyHybridify (fn, Promize) {
+  var self = this
+
+  function hybridified () {
+    hybridify.Promise = Promize || hybridifyHybridify.Promise || hybridified.Promise
+    return hybridify.apply(this || self, [fn].concat(utils.sliced(arguments)))
+  }
+  hybridified.__generatorFunction__ = fn
+
+  return hybridified
+}
+
+hybridify.promisify = function hybridifyPromisify (fn, Promize) {
+  utils.letta.promisify.Promise = Promize || hybridifyPromisify.Promise
+  return utils.letta.promisify.apply(this, arguments)
 }
---
+++ utils.js
@@ -0,0 +1,50 @@
+'use strict'
+
+/**
+ * Module dependencies
+ */
+
+var utils = require('lazy-cache')(require)
+
+/**
+ * Temporarily re-assign `require` to trick browserify and
+ * webpack into reconizing lazy dependencies.
+ *
+ * This tiny bit of ugliness has the huge dual advantage of
+ * only loading modules that are actually called at some
+ * point in the lifecycle of the application, whilst also
+ * allowing browserify and webpack to find modules that
+ * are depended on but never actually called.
+ */
+
+var fn = require
+require = utils // eslint-disable-line no-undef, no-native-reassign
+
+/**
+ * Lazily required module dependencies
+ */
+
+require('common-callback-names')
+require('handle-arguments', 'handle')
+require('letta')
+require('then-callback', 'then')
+require('sliced')
+
+/**
+ * Restore `require`
+ */
+
+require = fn // eslint-disable-line no-undef, no-native-reassign
+
+utils.normalizePromise = function normalizePromise (target, promise) {
+  target.Promise = promise.Promise
+  target.___customPromise = promise.___customPromise
+  target.___bluebirdPromise = promise.___bluebirdPromise
+  return target
+}
+
+/**
+ * Expose `utils` modules
+ */
+
+module.exports = utils
